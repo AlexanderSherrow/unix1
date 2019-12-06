7b7524d473db1de0663731e6b61630417a331a22
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-05 22:07:13 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -2,3 +2,7 @@ echo "Step <give number>"
 
 echo  "This is the input filename string:" <filename>
 wc -w $file > file/tmp/AlexSherrow
+
+mkdir AlexDirectory
+
+cp YourName ~/githubtask/unix/YourName_submit
