4a1a8a622a517b65ef2a15cb5ddab427c59431c9
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-06 00:52:47 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -5,15 +5,15 @@ echo "Step 2"
 echo  "This is the input filename string: $file"
 echo "Step 3"
 touch AlexSherrowWordCount
-wc -w file > AlexSherrowWordCount
+wc -w file > AlexSherrowWordCount.txt
 echo "Step 5"
-chmod 711  AlexSherrowWordCount
+chmod 711  AlexSherrowWordCount.txt
 echo "Step 6"
 mkdir AlexSherrow
 echo "Step 7"
 cd AlexSherrow || exit
 echo "Step 8"
-cp ALexSherrowWordCount .
+cp ALexSherrowWordCount.txt
 echo "Step 9 (DO LATER)"
 echo "Step 11"
 cp unix1/File_for_SHERROW_ALEX.sh .
