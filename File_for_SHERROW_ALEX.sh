b48bc966134bd75627a558e289bed82b0eb3a623
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-06 12:33:50 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Step 1"
-cd unix1
 file='Test'
 echo "Step 2"
 echo  "This is the input filename string: $file"
 echo "Step 3"
 mkdir tmp
 touch /tmp/AlexSherrow.txt
+pwd
 wc -w Test
 echo "Step 5"
 chmod 711  /tmp/AlexSherrow.txt
