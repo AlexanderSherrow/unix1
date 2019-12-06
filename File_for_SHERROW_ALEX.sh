121f9ce43502fa043a970f5916b0d2475bcdf4c9
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-06 00:20:42 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 echo "Step 1"
-
 echo "Step 2"
 echo  "This is the input filename string: $file"
-
 echo "Step 3"
 wc -w file > file/tmp/AlexSherrow
-
 echo "Step 5"
 chmod 711  file/tmp/AlexSherrow
-
 echo "Step 6"
 mkdir AlexDirectory
-
 echo "Step 7"
 cd AlexDirectory || exit
-
 echo "Step 8"
 cp file.doc newfile.doc
-
-
 cp YourName ~/githubtask/unix/YourName_submit
