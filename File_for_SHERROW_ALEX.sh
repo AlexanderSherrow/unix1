cb2ec93830f2ccc7edb9c0c2644af0249a4a9ee7
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-05 22:13:03 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -1,8 +1,20 @@
-echo "Step <give number>"
+#!/bin/bash
+echo "Step 1"
+file='filetest'
 
-echo  "This is the input filename string:" <filename>
+echo "Step 2"
+echo  "This is the input filename string:" $file
+
+echo "Step 3"
 wc -w $file > file/tmp/AlexSherrow
 
+echo "Step 5"
+chmod 711  file/tmp/AlexSherrow
+
+echo "Step 6"
 mkdir AlexDirectory
+
+echo "Step 7"
+cd AlexDirectory || exit
 
 cp YourName ~/githubtask/unix/YourName_submit
