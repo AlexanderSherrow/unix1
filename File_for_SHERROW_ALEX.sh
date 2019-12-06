4f02a13e75c97e32f5f7c25de311cffc4e60589b
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-06 00:39:54 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 echo "Step 1"
+file='filetest'
 echo "Step 2"
 echo  "This is the input filename string: $file"
 echo "Step 3"
-wc -w file > file/tmp/AlexSherrow
+wc -w file > file/tmp/AlexSherrowWordCount
 echo "Step 5"
-chmod 711  file/tmp/AlexSherrow
+chmod 711  file/tmp/AlexSherrowWordCount
 echo "Step 6"
-mkdir AlexDirectory
+mkdir AlexSherrow
 echo "Step 7"
 cd AlexDirectory || exit
 echo "Step 8"
-cp file.doc newfile.doc
+cp file/tmp/ALexSherrowWordCount .
+echo "Step 9 (DO LATER)"
+echo "Step 11"
+cp unix1/File_for_SHERROW_ALEX.sh .
+echo "Step 12"
+echo "Step 13"
+uuencode attachment-file | mail -s "Test" "sherrowa226@gmail.com"
+echo "Step 14"
 cp YourName ~/githubtask/unix/YourName_submit
