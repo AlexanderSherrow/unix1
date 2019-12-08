3f9e119d7c408474d89d429b97e960a1e34a6232
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-08 18:34:08 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -5,7 +5,7 @@ file=/home/tcht/unix1/Test
 echo "Step 2"
 echo  "This is the input filename string: $file"
 echo "Step 3"
-wc -w "$file" > /tmp/AlexSherrow.txt
+wc -w "$file" > /tmp/AlexSherrow
 echo "Step 4"
 cat $file $date > /tmp/AlexSherrow
 echo "Step 5"
@@ -19,12 +19,12 @@ cp /tmp/AlexSherrow .
 echo "Step 9"
 cp $file
 echo "Step 10"
-find / | grep /tmp/AlexSherrow > /tmp/AlexSherrow
+#find / | grep /tmp/AlexSherrow > /tmp/AlexSherrow
 echo "Step 11"
 cp /home/tcht/unix1/File_for_SHERROW_ALEX.sh .
 echo "Step 12"
 echo "Step 13"
 mail -s "ExamInfoFromAlexSherrow" tcht@iup.edu <<< "message"
-$ echo "Message Body Here" | mail -s "Subject Here" tcht@iup.edu -A /tmp/AlexSherrow.txt
+$ echo "Message Body Here" | mail -s "Subject Here" tcht@iup.edu -A /tmp/AlexSherrow
 echo "Step 14"
 cp /tmp/AlexSherrow home/tcht/unix1/AlexSherrow_submit
