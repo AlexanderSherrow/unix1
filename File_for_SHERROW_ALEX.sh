1a1fb7be3d255a3c258bddcb990390df99dfa536
AlexanderSherrow <51923534+AlexanderSherrow@users.noreply.github.com>
2019-12-08 18:04:05 -0500

    Update File_for_SHERROW_ALEX.sh

--- File_for_SHERROW_ALEX.sh
+++ File_for_SHERROW_ALEX.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
+perl -p -i -e "s/\r//g" File_for_SHERROW_ALEX.sh
 echo "Step 1"
 file=/home/tcht/unix1/Test
 echo "Step 2"
 echo  "This is the input filename string: $file"
 echo "Step 3"
-ls -1 $file
-wc -w "$file"
+wc -w "$file" > /tmp/AlexSherrow
 echo "Step 4"
-$date
-cat Test.txt $date > Test.txt
+cat $file $date > /tmp/AlexSherrow
 echo "Step 5"
-chmod 711  /tmp/AlexSherrow.txt
+chmod 711  /tmp/AlexSherrow
 echo "Step 6"
 mkdir AlexSherrow
 echo "Step 7"
